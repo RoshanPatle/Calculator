4ae5572447e149afdd7145a58c754499bfb5b9ee
roshan <patleroshan@gmail.com>
2023-08-22 08:42:33 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,56 @@
+'use strict'
+
+const input=document.getElementById('inputBox')
+const buttons=document.querySelectorAll('button')
+
+let string="";
+let arr=Array.from(buttons);
+ arr.forEach(button =>{
+    button.addEventListener('click' , function(e){
+        if(e.target.innerHTML == '='){
+            string=eval(string);
+            input.value=string;
+        }
+        else if(e.target.innerHTML== 'AC'){
+            string="";
+            input.value=string;
+        }
+        else if(e.target.innerHTML=='DEL'){
+            string=string.substring(0, string.length-1);
+            input.value=string;
+        }
+        
+        else{
+
+          input.value=string+= e.target.innerHTML;
+        }
+    })
+ })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
